59f8e5943f1c6ed8bfbe1f2bf59361b308566e42
ytkim4558 <ytkim4558@naver.com>
2016-12-26 23:04:12 +0900

    일부 한글 깨지는 문제 빼고 수정중

--- curlTest/curl_test.php
+++ curlTest/curl_test.php
@@ -1,11 +1,18 @@
 <?php
-echo '안녕';
-$ch = curl_init("http://item2.gmarket.co.kr/Item/DetailView/Item.aspx?goodscode=824231995");
-$fp = fopen("example_homepage.txt", "w");
+    // 참고 사이트 : http://www.jacobward.co.uk/web-scraping-with-php-curl-part-1/
+    // 기본 cURL 함수를 정의
+//    header('Content-type: text/html; charset=UTF-8');
+    function curl($url) {
+        $ch = curl_init();  // cURL 초기화
+        curl_setopt($ch, CURLOPT_URL, $url);    // Setting cURL's URL option with the $url variable passed into the function
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); // Setting cURL's option to return the webpage data
+        curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        $data = curl_exec($ch); // Executing the cURL request and assigning the returned data to the $data variable
+        curl_close($ch);    // Closing cURL
+        return $data;   // Returning the data from the function
+    }
 
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
+    echo '안녕';
+    $gmarket_page = curl("http://item2.gmarket.co.kr/Item/DetailView/Item.aspx?goodscode=824231995");
+    $gmarket_page_t = iconv("EUC-KR", "UTF-8", $gmarket_page);
+    echo $gmarket_page_t;
